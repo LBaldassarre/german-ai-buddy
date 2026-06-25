0b2bbcae32a92b0e40250939541f4f2232ce64da
lab-rdc <lbaldassarre.dev@gmail.com>
2026-06-25 19:08:13 +0200

    Simplify Chat page UI + enlarge the Side Pane

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -9,38 +9,17 @@ function Chat() {
     <>
         <div className="main-pane">
             <div className="chat-container">
-                <div className="cc-header">
-                    <div className="cc-header-title">
-                        Chat
-                    </div>
-                    <div className="cc-header-subtitle">
-                        Have a natural conversation in German
-                    </div>
-                </div>
-
                 <div className="cc-chat">
-                    <div className="cc-message-buddy">
-                        <div className="cc-message-buddy-lable">Buddy</div>
+                    <div className="cc-message buddy">
                         <div className="cc-message-buddy-content">Hallo! Wie ghet es dir?</div>
                         <div className="cc-message-buddy-translation">Hi! How are you?</div>
-                        <div className="cc-message-buddy-stats">
-                            <div className="cc-message-buddy-time">00:00</div>
-                            <div className="cc-message-buddy-send">
-                                <img src={checkIcon} alt="check" />
-                                <img src={checkIcon} alt="check" />
-                            </div>
-                        </div>
                     </div>
-                    <div className="cc-message-user">
-                        <div className="cc-message-user-lable">You</div>
+                    <div className="cc-message user">
                         <div className="cc-message-user-content">Ich bin gut und dir?</div>
-                        <div className="cc-message-user-stats">
-                            <div className="cc-message-user-time">00:00</div>
-                            <div className="cc-message-user-send">
-                                <img src={checkIcon} alt="check" />
-                                <img src={checkIcon} alt="check" />
-                            </div>
-                        </div>
+                    </div>
+                    <div className="cc-message buddy">
+                        <div className="cc-message-buddy-content">Ich bien auch gut, Danke!</div>
+                        <div className="cc-message-buddy-translation">I am fine too, thanks!</div>
                     </div>
                 </div>
 
